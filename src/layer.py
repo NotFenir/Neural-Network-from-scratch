35061a659fc2e6dc12ffb0cc557aa7629a5fe5e1
NotFenir <mwisnia24@gmail.com>
2026-02-04 23:22:13 +0100

    feat: weights and bias properties added

--- src/layer.py
+++ src/layer.py
@@ -9,5 +9,13 @@ class Layer:
         self.weights: np.ndarray = np.random.randn(input_dim, output_dim) * np.sqrt(2 / input_dim)
         self.bias: np.ndarray = np.zeros(output_dim)
         
+    @property
+    def weights(self):
+        return self.weights
+    
+    @property
+    def bias(self):
+        return self.bias
+        
     def __call__(self, X: np.ndarray) -> np.ndarray:
         return (X @ self.weights.T) + self.bias
