6f88da5720881c31c90d1567712884fff7b218c4
NotFenir <mwisnia24@gmail.com>
2026-02-04 17:36:06 +0100

    feat: adding basics of Layer class

---
+++ src/layer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Layer:
+    '''
+    Docstring for Layer
+    '''
+    
+    def __init__(self, input_dim: int, output_dim: int) -> None:
+        self.weights: np.ndarray = np.random.randn(input_dim, output_dim) * np.sqrt(2 / input_dim)
+        self.bias: np.ndarray = np.zeros(output_dim)
+        
+    def __call__(self, X: np.ndarray) -> np.ndarray:
+        return (X @ self.weights.T) + self.bias
