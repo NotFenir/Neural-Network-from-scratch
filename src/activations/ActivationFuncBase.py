b4fe03cd82e3b3033e0171fc76051a899f73a333
Milosz Wisniewski <mwisnia24@gmail.com>
2026-07-20 00:05:36 +0200

    refactor: rename file

--- src/activations/ActivationFuncBase.py
+++ src/activations/ActivationFuncBase.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
+
 import numpy as np
 
+from framework_types import Input, ModelPart
 from utils import validate_data_type
-from framework_types import Input
-
 
 ALLOWED_INPUT_TYPES = Input.__value__.__args__
 
 
-class ActivationFuncBase(ABC):
+class ActivationFuncBase(ABC, ModelPart):
     def __call__(self, x: Input) -> np.ndarray | np.float64:
         return self.calculate(x)
 
@@ -17,7 +17,11 @@ class ActivationFuncBase(ABC):
         pass
 
     @abstractmethod
-    def gradient(self, x: Input) -> np.ndarray | np.float64:
+    def gradient(self) -> np.ndarray | float:
+        pass
+
+    @abstractmethod
+    def calculate_gradient(self, x: Input) -> np.ndarray | np.float64:
         pass
 
     def validate_data(self, x: Input):
