53ffe2584cc5c0f133433f8528e12a78065d1039
Milosz Wisniewski <mwisnia24@gmail.com>
2026-07-19 22:17:47 +0200

    feat: add validate_data_type func to framework access

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,4 +4,4 @@ from .validate_data import validate_data_type
 from .validate_data import is_data_length_same
 from .validate_data import Input
 
-__all__ = ["validate_data", "Input"]
+__all__ = ["validate_data", "Input", "validate_data_type"]
