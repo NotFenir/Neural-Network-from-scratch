b3261c7c66ed0e157994b6ef4539d687162204ee
Milosz Wisniewski <mwisnia24@gmail.com>
2026-07-20 00:08:56 +0200

    feat: add forward pass, __init__ and init backward

--- src/model.py
+++ src/model.py
@@ -1,21 +1,42 @@
 import numpy as np
 
+from framework_types import ModelSequence
+from layer import Layer
+from activations import ActivationFuncBase
+
+
 class Model:
-    '''
+    """
     Docstring for Model
-    '''
-    
-    
-    def __init__(self, *args, **kwargs):
-        ...
-    
+    """
+
+    def __init__(self, sequence: ModelSequence):
+        self._sequence: ModelSequence = sequence
+        self._has_forwarded: bool = False
+
     def __call__(self, x: np.ndarray) -> np.ndarray:
-        return self.predict(x)
-    
-    
-    def fit(self) -> None:
-        ...
-        
-    def predict(self, x: np.ndarray) -> np.ndarray:
-        ...
-        
+        return self.forward(x)
+
+    def fit(self, X: np.ndarray, y: np.ndarray, epochs: int, optimizer) -> None:
+        pass
+
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        self._has_forwarded = True
+        x = self._sequence[0](X)
+
+        for layer in self._sequence[1:]:
+            x = layer(x)
+        return x
+
+    def backward(self, loss_value: float) -> None:
+        if not self._has_forwarded:
+            raise ValueError("There was no forward passed")
+
+        delta: np.ndarray | float = loss_value
+
+        for layer in self._sequence[::-1]:
+            ...
+            # if isinstance(layer, Layer):
+            #     print(layer.gradient_weights(delta))
+            # elif isinstance(layer, ActivationFuncBase):
+            #     print(layer.gradient())
