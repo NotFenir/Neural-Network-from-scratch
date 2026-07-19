94b9ef8075fe04631338dab8c7180e3d5f0ea1e3
Milosz Wisniewski <mwisnia24@gmail.com>
2026-07-20 00:08:15 +0200

    refactor: extend sigmodi class

--- src/activations/Sigmoid.py
+++ src/activations/Sigmoid.py
@@ -1,16 +1,25 @@
 import numpy as np
-from .ActivaionFuncBase import ActivationFuncBase
+
 from framework_types import Input
+
+from .ActivationFuncBase import ActivationFuncBase
 
 
 class Sigmoid(ActivationFuncBase):
-    def calculate(self, x: Input) -> np.ndarray | np.float64:
+    def __init__(self) -> None:
+        self._input_cache: np.ndarray = None
+
+    def calculate(self, x: Input) -> np.ndarray | float:
         x = self.validate_data(x)
+        self._input_cache = x
         return self._eval(x)
 
-    def gradient(self, x: Input) -> np.ndarray | np.float64:
+    def calculate_gradient(self, x: Input) -> np.ndarray | float:
         x = self.validate_data(x)
         return self._eval(x) * (1 - self._eval(x))
+
+    def gradient(self) -> np.ndarray | float:
+        return self._eval(self._input_cache) * (1 - self._eval(self._input_cache))
 
     @classmethod
     def _eval(cls, x: Input) -> np.ndarray | np.float64:
