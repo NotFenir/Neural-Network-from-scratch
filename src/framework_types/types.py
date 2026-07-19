12efaa99f8c1a70d5bc95e575f1364851180bddd
Milosz Wisniewski <mwisnia24@gmail.com>
2026-07-20 00:06:13 +0200

    feat: add new type

--- src/framework_types/types.py
+++ src/framework_types/types.py
@@ -1,3 +1,6 @@
 import numpy as np
 
+from .class_types import ModelPart
+
 type Input = np.ndarray | float | int | list | tuple | np.float64
+type ModelSequence = list[ModelPart] | tuple[ModelPart]
